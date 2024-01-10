6f17fd4b2f15841ccf687a0e0f5b7403abff9242
Justice <onyekaozuru122@gmail.com>
2024-01-09 16:09:55 -0800

    Task 1 Secure access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,7 @@
 # Write a function that retrieves an element from a list like in C.
 
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx > 0 and idx < len(my_list):
+        return my_list[idx]
+    else:
         return None
-    if idx > (len(my_list)):
-        return None
-    return my_list[idx]
