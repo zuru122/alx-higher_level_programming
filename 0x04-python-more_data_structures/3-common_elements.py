3f65b5b3e5e5364210fe7d9ae7e07952dec9782a
Justice <onyekaozuru122@gmail.com>
2024-01-16 12:58:16 -0800

    3. Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author: Sunday, Justice Gabriel
+"""
+Write a function that returns a set of common elements in two sets.
+Prototype: def common_elements(set_1, set_2):
+You are not allowed to import any module
+"""
+
+
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
