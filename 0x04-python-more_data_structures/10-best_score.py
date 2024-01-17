c9e91f3730893a202733ad41d0ec20e2be7addd1
Justice <onyekaozuru122@gmail.com>
2024-01-17 11:21:26 -0800

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Author: Sunday, Justice Gabriel
+"""
+Write a function that returns a key with the biggest integer value.
+
+Prototype: def best_score(a_dictionary):
+You can assume that all values are only integers
+If no score found, return None
+You can assume all students have a different score
+You are not allowed to import any module
+"""
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best_key = ''
+    large_num = next(iter(a_dictionary.values()))
+    for key in a_dictionary:
+        if a_dictionary[key] > large_num:
+            large_num = a_dictionary[key]
+            best_key = key
+    return best_key
