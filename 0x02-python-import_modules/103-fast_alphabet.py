90903a37b44b86cb9a7ddcff85225e16a1de138c
root <onyekaozuru122@gmail.com>
2024-03-14 19:34:52 -0700

    Advance Task 103-fast_alphabet.py

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#  Author: sunday, Justice Gabriel
+
+if __name__ == "__main__":
+    import string
+
+    uppercase_alphabet = string.ascii_uppercase
+    print(uppercase_alphabet)
