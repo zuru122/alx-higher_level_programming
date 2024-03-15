3c725ff83542fd4bc4d152d1366833f00dc3b11e
root <onyekaozuru122@gmail.com>
2024-03-14 19:40:50 -0700

    Task 103-fast_alphabet.py, made sure the codeline does nor exceed 3

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-#  Author: sunday, Justice Gabriel
-
-if __name__ == "__main__":
-    import string
-
-    uppercase_alphabet = string.ascii_uppercase
-    print(uppercase_alphabet)
+import string
+print(string.ascii_uppercase)
