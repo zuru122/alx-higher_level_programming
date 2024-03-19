2c44f91fc3acb3c7882a8c012da5962938e2b802
root <onyekaozuru122@gmail.com>
2024-03-18 19:04:44 -0700

    More  return

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author: Sunday, Justice Gabriel
+
+for i in range (90, 64, -1):
+    print(chr(i % 2 * 32 + i), end='')
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,9 @@
 # Author: Sunday, Justice Gabriel
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        first == None
+    if len(sentence) == '':
+        first = None
     else:
-        first  = sentence[0]
-    length =  len(sentence)
-    return  length, first
+        first = sentence[0]
+    length = len(sentence)
+    return length, first
